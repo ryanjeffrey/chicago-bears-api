d871fa0e36a4078af7020e3e1bc0821858bf767b
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-11-04 16:40:01 -0500

    Create test to return offense

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -41,6 +41,90 @@ describe('roster routes', () => {
     expect(res.body).toEqual(chaseClaypool);
   });
 
+  it('/roster/offense should return a list of the starting offense', async () => {
+    const res = await request(app).get('/roster/offense');
+    const offense = [
+      {
+        id: '1',
+        firstName: 'Justin',
+        lastName: 'Fields',
+        number: 1,
+        position: 'QB',
+      },
+      {
+        id: '2',
+        firstName: 'Darnell',
+        lastName: 'Mooney',
+        number: 11,
+        position: 'WR',
+      },
+      {
+        id: '3',
+        firstName: 'Chase',
+        lastName: 'Claypool',
+        number: 10,
+        position: 'WR',
+      },
+      {
+        id: '4',
+        firstName: 'David',
+        lastName: 'Montgomery',
+        number: 32,
+        position: 'RB',
+      },
+      {
+        id: '5',
+        firstName: 'Khari',
+        lastName: 'Blasingame',
+        number: 35,
+        position: 'FB',
+      },
+      {
+        id: '6',
+        firstName: 'Cole',
+        lastName: 'Kmet',
+        number: 85,
+        position: 'TE',
+      },
+      {
+        id: '7',
+        firstName: 'Braxton',
+        lastName: 'Jones',
+        number: 70,
+        position: 'LT',
+      },
+      {
+        id: '8',
+        firstName: 'Cody',
+        lastName: 'Whitehair',
+        number: 65,
+        position: 'LG',
+      },
+      {
+        id: '9',
+        firstName: 'Lucas',
+        lastName: 'Patrick',
+        number: 62,
+        position: 'C',
+      },
+      {
+        id: '10',
+        firstName: 'Teven',
+        lastName: 'Jenkins',
+        number: 76,
+        position: 'RG',
+      },
+      {
+        id: '11',
+        firstName: 'Larry',
+        lastName: 'Borom',
+        number: 75,
+        position: 'RT',
+      },
+    ];
+    expect(res.body).toEqual(offense);
+  });
+
   afterAll(() => {
     pool.end();
   });
