8e181e04f23dd8feec47ec499d826a4f5cf08342
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-11-04 14:38:52 -0500

    Update test with roster data

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,18 +3,20 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+const { roster } = require('../lib/roster-data');
+
+describe('roster routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  
+
   it('/roster should return a list of players on roster', async () => {
     const res = await request(app).get('/roster');
     const expected = roster.map((player) => {
       return {
         number: player.number,
-        firstName: player.first_name,
-        lastName: player.last_name,
+        firstName: player.firstName,
+        lastName: player.lastName,
       };
     });
     expect(res.body).toEqual(expected);
