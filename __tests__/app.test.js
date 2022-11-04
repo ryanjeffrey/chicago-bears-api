e4f83b22d3c5fc944208b07375ab66a15e0f8229
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-11-04 14:09:29 -0500

    Create test to return list of players on roster

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,25 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  
+  it('/roster should return a list of players on roster', async () => {
+    const res = await request(app).get('/roster');
+    const expected = roster.map((player) => {
+      return {
+        number: player.number,
+        firstName: player.first_name,
+        lastName: player.last_name,
+      };
+    });
+    expect(res.body).toEqual(expected);
   });
+
   afterAll(() => {
     pool.end();
   });
